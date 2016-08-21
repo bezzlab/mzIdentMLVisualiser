cf4751cc723c2595de8cda88bb1e78576b4e3091
sureshhewabi <sureshhewabi@gmail.com>
2016-08-21 11:30:52 +0100

    modified

--- test/PRIDE_filesize_analysis.R
+++ test/PRIDE_filesize_analysis.R
@@ -8,7 +8,7 @@
 # -----------------------------------------------------------------------------------------
 
 # Load data from CSV File
-mzid <- read.csv(file="/Users/sureshhewapathirana/Documents/Projects/ResearchProject/mzIdentMLVisualiser/test/mzIdentML_12_08_2016.csv", header=TRUE, sep=",")
+mzid <- read.csv(file="/Users/sureshhewapathirana/Documents/Projects/ResearchProject/mzIdentMLVisualiser/test/mzIdentML_12_08_2016_mzid.csv", header=TRUE, sep=",")
 
 # Do not use scientific number format, use floating numbers
 options(scipen = 999)
@@ -17,19 +17,24 @@ options(scipen = 999)
 View(mzid)
 str(mzid)
 
+# data convert to MB
+mzid[,"MB"] <- mzid$Size.Bytes./1000000
+
 # basic statistical analysis 
-mean(mzid$Size.Bytes./1000000)
-median(mzid$Size.Bytes./1000000)
-min(mzid$Size.Bytes./1000000)
-max(mzid$Size.Bytes./1000000)
+mean(mzid$MB)
+median(mzid$MB)
+min(mzid$MB)
+max(mzid$MB)
 
 # largets  and smallest file size records
-mzid[which((mzid$Size.Bytes. == min(mzid$Size.Bytes.))==TRUE),]
-mzid[which((mzid$Size.Bytes. == max(mzid$Size.Bytes.))==TRUE),]
+mzid[which((mzid$MB == min(mzid$MB))==TRUE),]
+mzid[which((mzid$MB == max(mzid$MB))==TRUE),]
 
 
 # total number of files
 nrow(mzid)
+# total number of files smaller than 1 GB
+nrow(mzid[which(mzid$MB < 500),])
 
 # ---------------- Data are grouped by experiment [BYTES] ---------------- 
 
@@ -37,6 +42,8 @@ data_by_experiment <- (aggregate(mzid[,2:4], list(mzid$SubmissionId), FUN=mean))
 #data_by_experiment$Year <- round(data_by_experiment$Year,0)
 View(data_by_experiment)
 nrow(data_by_experiment)
+
+mean(data_by_experiment$Size.Bytes.)/1000000
 
 # histograms for data grouped by experiment
 hist(data_by_experiment$Size.Bytes./1000000, breaks = 100, main = "MzIdentML File Sizes Distribution", xlab = "File size(Bytes)", ylab = "Number of files")
@@ -68,6 +75,18 @@ hist(log(data_by_experiment$x), breaks = 100, main = "Histogram of mzIdentML Fil
 
 
 # get records from a specific range
+View(data_by_experiment[which(data_by_experiment$Size.Bytes.>500000000 & data_by_experiment$Size.Bytes.<599000000),])
+View(data_by_experiment[which(data_by_experiment$Size.Bytes.>600000000 & data_by_experiment$Size.Bytes.<699000000),])
+View(data_by_experiment[which(data_by_experiment$Size.Bytes.>700000000 & data_by_experiment$Size.Bytes.<799000000),])
+View(data_by_experiment[which(data_by_experiment$Size.Bytes.>800000000 & data_by_experiment$Size.Bytes.<899000000),])
+View(data_by_experiment[which(data_by_experiment$Size.Bytes.>900000000 & data_by_experiment$Size.Bytes.<999000000),])
+# or
+View(data_by_experiment[which(mzid$Size.MB.>500 & mzid$Size.MB.<999),])
+View(data_by_experiment[which(mzid$Size.MB.>220 & mzid$Size.MB.<240),])
+
+
+
+# get records from a specific range
 View(mzid[which(mzid$Size.MB.>500 & mzid$Size.MB.<599),])
 View(mzid[which(mzid$Size.MB.>600 & mzid$Size.MB.<699),])
 View(mzid[which(mzid$Size.MB.>700 & mzid$Size.MB.<799),])
@@ -78,5 +97,5 @@ View(mzid[which(mzid$Size.MB.>500 & mzid$Size.MB.<999),])
 View(mzid[which(mzid$Size.MB.>220 & mzid$Size.MB.<240),])
 
 year_average <- aggregate(data_by_experiment[,4,5], list(data_by_experiment$Year), FUN=sum)
-barplot(year_average))
+
 
